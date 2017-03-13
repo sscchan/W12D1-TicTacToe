dbf4794a647de5620cdc8fe997e01243e60d19a9
sscchan <stephen.s.c.chan@gmail.com>
2017-03-13 12:10:41 -0700

    implement Board winner detection

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,5 @@
+const clearScreen = require('./ClearScreen.js');
+
 class Board {
   constructor(sizeX, sizeY) {
 
@@ -9,6 +11,8 @@ class Board {
     this.reset.bind(this);
     this.get.bind(this);
     this.set.bind(this);
+    this.isValidMove.bind(this);
+    this.getWinner.bind(this);
 
     this.reset();
   }
@@ -16,31 +20,128 @@ class Board {
   // This reset board's state array elements to empty string ""
   reset() {
     for (let i = 0; i < this.sizeX; i++) {
+      this.boardState[i] = [];
       for (let j = 0; j < this.sizeY; j++) {
-        this.boardState[i, j] = '';
+        this.boardState[i].push(' ');
       }
     }
   }
 
   get(x, y) {
-    return this.boardState[x, y];
+    return this.boardState[x][y];
   }
 
   // Set a particular position of the board with a symbol
   set(x, y, symbol) {
-    if (this.get(x, y) === '') {
-      this.boardState[x, y] = symbol;
+    if (this.isValidMove(x, y)) {
+      this.boardState[x][y] = symbol;
     } else {
       // throw error
     }
+  }
+
+  isValidMove(x, y) {
+    return (this.get(x, y) === ' ');
+  }
+
+  // Check if there are any winners on this round
+  getWinner() {
+
+    // Check horizontal for winners
+    for (let i = 0; i < this.sizeX; i++) {
+      let currentRow = this.boardState[i];
+      let lastSymbol = currentRow[0];
+      let foundWinner = true;
+
+      for (let j = 0; j < currentRow.length; j++) {
+        if (currentRow[j] === ' ') {
+          foundWinner = false;
+          break;
+        } else {
+          if (currentRow[j] !== lastSymbol) {
+            foundWinner = false;
+            break;
+          }
+        }
+      }
+
+      if (foundWinner) {
+        return lastSymbol;
+      }
+    }
+
+    // Check vertical for winners
+    for (let i = 0; i < this.sizeY; i++) {
+      let lastSymbol = this.boardState[0][i];
+      let foundWinner = true;
+
+      for (let j = 0; j < this.sizeX; j++) {
+        if (this.boardState[j][i] === ' ') {
+          foundWinner = false;
+          break;
+        } else {
+          if (this.boardState[j][i] !== lastSymbol) {
+            foundWinner = false;
+            break;
+          }
+        }
+      }
+
+      if (foundWinner) {
+        return lastSymbol;
+      }
+    }
+
+    // Check diagonal for winners
+    let foundWinner = true;
+    let lastSymbol = this.boardState[0][0];    
+    for (let i = 0; i < this.sizeX; i++) {
+      if (this.boardState[i][i] === ' ') {
+        foundWinner = false;
+        break;
+      }
+
+      if (this.boardState[i][i] !== lastSymbol) {
+        foundWinner = false;
+        break;
+      }
+    }
+
+    if (foundWinner) {
+      return lastSymbol;
+    }
+
+    // Check diagonal (backwards) for winners
+    foundWinner = true;
+    lastSymbol = this.boardState[0][this.sizeY - 1];    
+    for (let i = 0; i < this.sizeX; i++) {
+      if (this.boardState[i][this.sizeY - 1 - i] === ' ') {
+        foundWinner = false;
+        break;
+      }
+
+      if (this.boardState[i][this.sizeY - 1 - i] !== lastSymbol) {
+        foundWinner = false;
+        break;
+      }
+    }
+    
+    if (foundWinner) {
+      return lastSymbol;
+    }    
+
+
+    return null;
   }
 
   getRenderedBoard() {
     let renderedBoardText = '';
 
     for (let i = 0; i < this.sizeX; i++) {
-      console.log(this.boardState[i].join('|'));
+      renderedBoardText += ('|' + this.boardState[i].join(',') + '|\n');
     }
+
+    return renderedBoardText;
   }
 }
 
