ad955ff865c041d3848774af22c39dee26fdbbad
sscchan <stephen.s.c.chan@gmail.com>
2017-03-13 12:11:03 -0700

    implemented game play process

--- src/Game.js
+++ src/Game.js
@@ -11,10 +11,52 @@ class Game {
     this.currentPlayer = this.player1;
 
     this.startGame.bind(this);
+    this.nextMove.bind(this);
+    this.switchActivePlayer.bind(this);
+
+
+    this.startGame();
   }
 
   startGame() {
     this.board.reset();
+    this.nextMove();
+  }
+
+  nextMove() {
+    let handleMoveRecieved = (data) => {
+      if (this.board.isValidMove(data[0], data[1])) {
+        this.board.set(data[0], data[1], this.currentPlayer.symbol);
+
+        // Print out the board
+        console.log(this.board.getRenderedBoard());
+
+        // Check if there are any winner
+        if (this.board.getWinner() === null) {
+          this.switchActivePlayer();
+          this.nextMove();
+        } else {
+          console.log('Winner is:', this.board.getWinner());
+        }
+
+      } else {
+        console.log('Invalid Move, Please ReEnter');
+        this.nextMove();
+      }
+    };
+
+    this.currentPlayer.getMove(handleMoveRecieved);
+
+  }
+
+
+
+  switchActivePlayer() {
+    if (this.currentPlayer === this.player1) {
+      this.currentPlayer = this.player2;
+    } else {
+      this.currentPlayer = this.player1;
+    }
   }
 }
 
