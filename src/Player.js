3011bd33e1401755006d84a4d5c98211a22038e3
sscchan <stephen.s.c.chan@gmail.com>
2017-03-13 10:37:57 -0700

    add skeleton for Player class

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import prompt from 'prompt';
+
+export default class Player {
+  constructor(name, symbol) {
+   this.name = name;
+   this.symbol = symbol; 
+  }
+
+  getMove() {
+    // Prompt user for the next move
+  }
+}
