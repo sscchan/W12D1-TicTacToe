7bc5c137b3d2c42ba1694491ed25c9db4e972930
sscchan <stephen.s.c.chan@gmail.com>
2017-03-13 12:11:25 -0700

    add Player prompt for input

--- src/Player.js
+++ src/Player.js
@@ -4,10 +4,36 @@ class Player {
   constructor(name, symbol) {
     this.name = name;
     this.symbol = symbol; 
+
+    this.getMove.bind(this);
+    this.parseUserMoveinput.bind(this);
   }
 
-  getMove() {
+  getMove(onMoveRecieve) {
     // Prompt user for the next move
+    console.log(this.name, ': Please Enter Your Move in the form of X, Y');
+    prompt.start();
+
+    prompt.get(['move'], (error, result) => {
+      if (error) {
+        onMoveRecieve(error);
+      }
+
+      onMoveRecieve(this.parseUserMoveinput(result.move));
+    });
+  }
+
+  parseUserMoveinput(rawInput) {
+    // Remove all whitespaces
+    let conditionedInput = rawInput.replace(/\s/, '');
+    let moveArray = conditionedInput.split(',');
+    console.log('parseUserMoveInput: moveArray', moveArray);
+    // See if we have 2 values
+    if (moveArray.length !== 2) {
+      throw 'Invalid User Input';
+    } else {
+      return moveArray;
+    }
   }
 }
 
